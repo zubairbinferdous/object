886209dde4d6c1454f68cf24df98e91ed208ac8e
zubairbinferdous <zubairbinf@gmail.com>
2021-11-07 00:25:11 +0600

    ok

---
+++ root.js
@@ -0,0 +1,7 @@
+// find the Area of a Triangle
+let a = 5;
+let b = 6;
+let c = 7;
+let s = (a + b + c) / 2;
+let area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+console.log(area);
--- runtime.js
+++ runtime.js
@@ -1,1 +1,2 @@
 console.log(1223);
+let name = 'zubair bin ferdous';
