533b52113b5e756f60f5e8298b8610f429d58716
zubairbinferdous <zubairbinf@gmail.com>
2021-11-11 20:13:35 +0600

    ok done

--- runtime.js
+++ runtime.js
@@ -1,4 +1,9 @@
 console.log(1223);
 console.log(4444);
 console.log(5555);
-let name = 'zubair bin ferdous';
+
+// let Name = 'zubair bin ferdous';
+// console.log(Name);
+// for (let i = 0; i <= 1000; i++) {
+//     console.log(i)
+// }
